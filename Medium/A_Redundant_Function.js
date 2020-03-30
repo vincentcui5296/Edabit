8c2deef306278047f045ac24b3b770b204ec35ee
vincentcui5296 <30999344+vincentcui5296@users.noreply.github.com>
2020-03-30 14:27:36 -0600

    Update A_Redundant_Function.js

--- Medium/A_Redundant_Function.js
+++ Medium/A_Redundant_Function.js
@@ -1,14 +1,14 @@
 /**
  * A Redundant Function
  * Write a function redundant that takes in a string str and returns a function that returns str.
-
+ *
  * Examples
  * const f1 = redundant("apple")
  * f1() ➞ "apple"
-
+ *
  * const f2 = redundant("pear")
  * f2() ➞ "pear"
-
+ *
  * const f3 = redundant("")
  * f3() ➞ ""
  * Notes
@@ -18,7 +18,7 @@
  * const f1 = redundant("apple")
  * const f2 = redundant("pear")
  * const f3 = redundant("")
-
+ *
  * Test.assertEquals(f1(), "apple")
  * Test.assertEquals(f2(), "pear")
  * Test.assertEquals(f3(), "")
