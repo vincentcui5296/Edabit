ad1f84578a348b8c478015827e74e41ca9aab7fd
vincentcui5296 <30999344+vincentcui5296@users.noreply.github.com>
2020-03-30 17:46:05 -0600

    Create Convert_Key_Values_in_an_Object_to_Array.js

---
+++ Medium/Convert_Key_Values_in_an_Object_to_Array.js
@@ -0,0 +1,35 @@
+/**
+ * Convert Key, Values in an Object to Array
+ * Write a function that converts an object into an array of keys and values.
+ * 
+ * Examples
+ * objectToArray({
+ *   D: 1,
+ *   B: 2,
+ *   C: 3
+ * }) ➞ [["D", 1], ["B", 2], ["C", 3]]
+ * 
+ * objectToArray({
+ *   likes: 2,
+ *   dislikes: 3,
+ *   followers: 10
+ * }) ➞ [["likes", 2], ["dislikes", 3], ["followers", 10]]
+ * Notes
+ * N/A
+ * 
+ * Test.assertSimilar(objectToArray({
+ * 	D: 1, 
+ *   B: 2, 
+ *   C: 3
+ * }), [["D", 1], ["B", 2], ["C", 3]])
+ * 
+ * Test.assertSimilar(objectToArray({
+ * 	likes: 2, 
+ *   dislikes: 3, 
+ *   followers: 10
+ * }), [["likes", 2], ["dislikes", 3], ["followers", 10]])
+ */
+
+function objectToArray(obj) {
+	return Object.entries(obj)
+}
