2679e1d1c0f288ab42823ef22ff483bec46acbe8
vincentcui5296 <30999344+vincentcui5296@users.noreply.github.com>
2020-03-31 00:54:22 -0600

    Create Concatenating_Two_Integer_Arrays.js

---
+++ Very_Easy/Concatenating_Two_Integer_Arrays.js
@@ -0,0 +1,22 @@
+/**
+ * Concatenating Two Integer Arrays
+ * Create a function to concatenate two integer arrays.
+ * 
+ * Examples
+ * concat([1, 3, 5], [2, 6, 8]) ➞ [1, 3, 5, 2, 6, 8]
+ * 
+ * concat([7, 8], [10, 9, 1, 1, 2]) ➞ [7, 8, 10, 9, 1, 1, 2]
+ * 
+ * concat([4, 5, 1], [3, 3, 3, 3, 3]) ➞ [4, 5, 1, 3, 3, 3, 3, 3]
+ * Notes
+ * Don't forget to return the result.
+ * See Resources tab for more info.
+ * 
+ * Test.assertSimilar(concat([1, 3, 5], [2, 6, 8]), [1, 3, 5, 2, 6, 8])
+ * Test.assertSimilar(concat([7, 8], [10, 9, 1, 1, 2]), [7, 8, 10, 9, 1, 1, 2])
+ * Test.assertSimilar(concat([4, 5, 1], [3, 3, 3, 3, 3]), [4, 5, 1, 3, 3, 3, 3, 3])
+ */
+
+function concat(arr1, arr2) {
+	return [...arr1, ...arr2]
+}
