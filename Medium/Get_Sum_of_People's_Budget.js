94912eae18c88d873dab03b71e09e4cb5ce2e702
vincentcui5296 <30999344+vincentcui5296@users.noreply.github.com>
2020-03-30 20:41:36 -0600

    Update Get_Sum_of_People's_Budget.js

--- Medium/Get_Sum_of_People's_Budget.js
+++ Medium/Get_Sum_of_People's_Budget.js
@@ -16,6 +16,11 @@
  * ]) ➞ 62600
  * Notes
  * N/A
+ *
+ * Test.assertEquals(getBudgets([{name: "John",  age: 21, budget: 23000}, {name: "Steve",  age: 32, budget: 40000}, {name: "Martin",  age: 16, budget: 2700}]), 65700)
+ * Test.assertEquals(getBudgets([{name: "John",  age: 21, budget: 29000}, {name: "Steve",  age: 32, budget: 32000}, {name: "Martin",  age: 16, budget: 1600}]), 62600)
+ * Test.assertEquals(getBudgets([{name: "John",  age: 21, budget: 19401}, {name: "Steve",  age: 32, budget: 12321}, {name: "Martin",  age: 16, budget: 1204}]), 32926)
+ * Test.assertEquals(getBudgets([{name: "John",  age: 21, budget: 10234}, {name: "Steve",  age: 32, budget: 21754}, {name: "Martin",  age: 16, budget: 4935}]), 36923)
  */
 
 function getBudgets(arr) {
